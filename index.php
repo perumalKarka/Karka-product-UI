d98bedafb736a99f14f2bcf54671bad426e5705c
hariajithkumar <ajithkumar@mail.comorins.com>
2022-08-16 17:47:17 +0530

    Branch-KP-1
    KP-1 completed

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <title>Hyper - Responsive Bootstrap 4 Admin Dashboard</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta content="A fully featured admin theme which can be used to build CRM, CMS, etc." name="description" />
+        <meta content="Coderthemes" name="author" />
+        <!-- App favicon -->
+        <link rel="shortcut icon" href="assets/images/favicon.ico">
+
+
+        <!-- App css -->
+        <link href="assets/css/icons.min.css" rel="stylesheet" type="text/css" />
+        <link href="assets/css/app-min.css" rel="stylesheet" type="text/css" />
+        <!-- third party css -->
+        <link href="assets/css/style.css" rel="stylesheet" type="text/css" />
+
+    </head>
+    <body class="auth-fluid-pages pb-0">
+
+        <?php include 'includes/nav.php'; ?>
+        <?php include 'includes/aside.php'; ?>
+        <?php include 'includes/footer.php'; ?>
+
+
+    </body>
+     <!-- App js -->
+     <script src="assets/js/app.min.js"></script>
+
+</html>
